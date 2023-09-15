0846f2d992d478a1208846fc84fb4ac822522e4b
Yasunori Takahashi <hibriiiiidge@gmail.com>
2023-09-16 06:32:26 +0900

    Change app bar text

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -21,7 +21,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Flutter x Firebase'),
       ),
       body: Center(
         child: Column(
